e1009fcb40b9b4500f2aa22d13d3e9096fe37ffe
sudeepbhandari6545 <sudeepbhandari6545@gmail.com>
2021-09-14 15:56:19 +0545

    airbnb

---
+++ components/LargeCard.js
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+
+function LargeCard({ img, title, description, buttonText }) {
+  return (
+    <section className="relative py-10 cursor-pointer">
+      <div className="relative h-96 min-w-[300px]">
+        <Image
+          src={img}
+          layout="fill"
+          objectFit="cover"
+          className="rounded-2xl"
+        />
+      </div>
+      <div className="absolute top-32 left-12">
+        <h3 className="text-4xl w-60 mb-3">{title}</h3>
+        <p>{description}</p>
+        <button className="text-sm bg-gray-900 text-white px-4 py-2 rounded-lg mt-5">
+          {buttonText}
+        </button>
+      </div>
+    </section>
+  )
+}
+
+export default LargeCard
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import LargeCard from '../components/LargeCard'
 import MediumCard from '../components/MediumCard'
 import SmallCard from '../components/SmallCard'
 
@@ -32,12 +33,18 @@ export default function Home({ exploreData, cardData }) {
         {/* medium card section */}
         <section>
           <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
-          <div className="flex space-x-3">
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
             {cardData?.map(({ img, title }) => (
               <MediumCard key={img} img={img} title={title} />
             ))}
           </div>
         </section>
+        <LargeCard
+          img="https://links.papareact.com/4cj"
+          title="The Greatest Outdoors"
+          description="Wishlists curated by Airbnb"
+          buttonText="Get Inspired"
+        />
       </main>
     </div>
   )
